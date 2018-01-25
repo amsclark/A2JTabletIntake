04ed902a02f235e7e253b284c78ec34a52693ce6
Alex Clark <amsclark@gmail.com>
2018-01-25 08:00:49 -0600

    Initial Commit

---
+++ process.php
@@ -0,0 +1,9 @@
+<?php
+echo "<pre>";
+echo "Here is what was posted to the The Processor\n";
+print_r($_POST);
+echo "And here is what would be re-posted to the lsxml transfer interface\n";
+echo "</pre>";
+
+
+?>
